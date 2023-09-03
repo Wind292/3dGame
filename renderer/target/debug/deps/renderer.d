e4140b8cdbb5521fd50adc86f45c91f573cce8dc
Luqcid71 <111896310+Luqcid71@users.noreply.github.com>
2023-09-03 16:30:49 -0400

    setup rust project

---
+++ renderer/target/debug/deps/renderer.d
@@ -0,0 +1,5 @@
+C:\Users\benja\OneDrive\Documents\GitHub\3dGame\renderer\target\debug\deps\renderer.exe: src\main.rs
+
+C:\Users\benja\OneDrive\Documents\GitHub\3dGame\renderer\target\debug\deps\renderer.d: src\main.rs
+
+src\main.rs:
---
+++ renderer/target/debug/renderer.d
@@ -0,0 +1,1 @@
+C:\Users\benja\OneDrive\Documents\GitHub\3dGame\renderer\target\debug\renderer.exe: C:\Users\benja\OneDrive\Documents\GitHub\3dGame\renderer\src\main.rs
